efd2d159b059ba1fa1b179c3f8aabaa46cec74f2
Brandon Winkler <brandonwinkler@live.ca>
2022-01-31 18:12:38 -0500

    working google search
    modal opens, searches selected activity, displays first activity shown in a container below

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+// Opens Modal on click of "search topic" button
+$(document).ready(function(){
+    $("#modBtn").click(function(){
+      $("#searchAct").modal();
+    });
+  });
+  
+  
+  // Closes modal on click of "search" button
+  $('#saveSearch').click(function(e) {
+    e.preventDefault();
+    $('#searchAct').modal('toggle');
+    return false;
+  });
+
+
+
+
+
+
+  $('#saveSearch').on('click', function(e){
+  e.preventDefault()
+  var query = $("#searchQuery").val()
+  let nearMe = "activities";
+  let queryA = query + nearMe;
+  var API_KEY = '93b9743008322f7aa1476f093145a2fb'
+  let result=''
+  var url = 'http://api.serpstack.com/search?access_key='  + API_KEY + "&type=web&num=1&query=" +queryA
+  console.log(url);
+  $.get(url, function(data){
+      $("#result").html('')
+      console.log(data)
+      data.organic_results.forEach(res => {
+          result =`
+          <h1>${res.title}</h1><br><a target="_blank" href="${res.url}">${res.url}</a>
+          <p>${res.snippet}</p>
+          `
+          $("#result").append(result)
+      });
+  })
+})
