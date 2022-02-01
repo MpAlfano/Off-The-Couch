561b7fd3f35c1eb9342e5a41b911a9408b2ef5ae
Mark Alfano <mpalfano4@gmail.com>
2022-02-01 12:50:06 -0500

    small changes to test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,82 @@
-let apiKey = "4351effe40295aa6c5165a4f79c80fdd";
-var searchVal = document.querySelector('#searchQuery'); //search input
-let searchRender = document.querySelector('#saveSearch') // search button in modal
-let searchChoice = ""
-let resultVal = document.querySelector('#results') // testing result
+// Opens Modal on click of "search topic" button
+$(document).ready(function(){
+    $("#modBtn").click(function(){
+      $("#searchAct").modal();
+    });
+  });
+  
+  
+  // Closes modal on click of "search" button
+  $('#saveSearch').click(function(e) {
+    e.preventDefault();
+    $('#searchAct').modal('toggle');
+    return false;
+  });
 
-navigator.geolocation.getCurrentPosition((position) => {
+  
+  navigator.geolocation.getCurrentPosition((position) => {
     const latitude  = position.coords.latitude;
     const longitude = position.coords.longitude;
     console.log(latitude)
     console.log(longitude)
   });
 
+  //93b9743008322f7aa1476f093145a2fb
 
-let searchActivity = function() {
-    console.log(searchChoice)
-    let apiUrl = "http://api.serpstack.com/search?access_key=" + apiKey + "&query=" + searchChoice
+//Selected Activity using serpstackAPI(uses google search)
+//use res. "console.log ID"
+//Grabbing from organic_results
+//Search Button on click
+  $('#saveSearch').on('click', function(e){
+  e.preventDefault()
+  var query = $("#searchQuery").val()  //Grabs value from searchQuery ID in HTML
+  let activities = "activities"; //Adds "activities" to the search
+  let queryA = query + activities; //What were actually searching
+  var API_KEY = '4351effe40295aa6c5165a4f79c80fdd' //serpstackAPI KEY
+  let result=''
+  var url = 'http://api.serpstack.com/search?access_key='  + API_KEY + "&type=web&num=1&query=" + query + "toronto"
+  console.log(url);
+  $.get(url, function(data){
+      $("#result").html('')
+      console.log(data)
+      data.organic_results.forEach(res => {
+        //What will be displayed
+          result =`
+          <h3>${res.title}</h3><br><a target="_blank" href="${res.url}">${res.url}</a>
+          <p>${res.snippet}</p>
+          `
+          //Appends to #result in HTML
+          $("#result").append(result)
+      });
+  });
+});
 
-    fetch(apiUrl)
-
-        .then(function (response) {
-
-            if (!response.ok) {
-                throw new Error('Network response not OK')
-            }
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data)
-            console.log(data.organic_results[0].title)
-            resultVal.textContent = data.organic_results[0].title
-        })
-
-}   // Make a forEach to produce and attach results maybe?
 
 
+// Random activity using BoredAPI
+// use data."console.log ID"
+let boredUrl = "https://www.boredapi.com/api/activity/"
 
-searchRender.addEventListener("click", function (event) {
-    event.preventDefault
+//Random button on click
+$('#randomQ').on('click', function(e){
+  e.preventDefault()
+  fetch(boredUrl)
+    .then(function (response) {
+      console.log(response);
+      if (!response.ok) {
+        throw response.json();
+      }
+      console.log(response.json);
+      return response.json();
 
-    $('#searchAct').modal('toggle')
-    searchChoice = searchVal.value
+    })
+    .then(function (data) {
+      console.log(data);
+      //What will be displayed
+      resultRandom = `<h3>${data.activity}</h3><p><a target="_blank" href="${data.link}">${data.link}</a></p>`
 
-    searchActivity()
-})
+      //Displays in #result in HTML
+      document.getElementById("result").innerHTML = resultRandom;
+      
+});
+});
