1214bc58d78a0004107986535bd942e14699693f
Brandon Winkler <brandonwinkler@live.ca>
2022-02-01 09:59:20 -0500

    geolocation added

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,13 @@ $(document).ready(function(){
     return false;
   });
 
-
+  
+  navigator.geolocation.getCurrentPosition((position) => {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude)
+    console.log(longitude)
+  });
 
 
 
