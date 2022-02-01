6fc1ad3c7618b5763415df0e9e6ea5da65ff8ba8
Brandon Winkler <brandonwinkler@live.ca>
2022-01-31 20:05:54 -0500

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,12 +18,15 @@ $(document).ready(function(){
 
 
 //Selected Activity using serpstackAPI(uses google search)
+//use res. "console.log ID"
+//Grabbing from organic_results
+//Search Button on click
   $('#saveSearch').on('click', function(e){
   e.preventDefault()
-  var query = $("#searchQuery").val()
-  let nearMe = "activities";
-  let queryA = query + nearMe;
-  var API_KEY = '93b9743008322f7aa1476f093145a2fb'
+  var query = $("#searchQuery").val()  //Grabs value from searchQuery ID in HTML
+  let activities = "activities"; //Adds "activities" to the search
+  let queryA = query + activities; //What were actually searching
+  var API_KEY = '93b9743008322f7aa1476f093145a2fb' //serpstackAPI KEY
   let result=''
   var url = 'http://api.serpstack.com/search?access_key='  + API_KEY + "&type=web&num=1&query=" +queryA
   console.log(url);
@@ -31,10 +34,12 @@ $(document).ready(function(){
       $("#result").html('')
       console.log(data)
       data.organic_results.forEach(res => {
+        //What will be displayed
           result =`
-          <h1>${res.title}</h1><br><a target="_blank" href="${res.url}">${res.url}</a>
+          <h3>${res.title}</h3><br><a target="_blank" href="${res.url}">${res.url}</a>
           <p>${res.snippet}</p>
           `
+          //Appends to #result in HTML
           $("#result").append(result)
       });
   });
@@ -43,8 +48,10 @@ $(document).ready(function(){
 
 
 // Random activity using BoredAPI
+// use data."console.log ID"
 let boredUrl = "https://www.boredapi.com/api/activity/"
 
+//Random button on click
 $('#randomQ').on('click', function(e){
   e.preventDefault()
   fetch(boredUrl)
@@ -59,7 +66,10 @@ $('#randomQ').on('click', function(e){
     })
     .then(function (data) {
       console.log(data);
-      resultRandom = `<p>${data.activity}</p><p><a target="_blank" href="${data.link}">${data.link}</a></p>`
+      //What will be displayed
+      resultRandom = `<h3>${data.activity}</h3><p><a target="_blank" href="${data.link}">${data.link}</a></p>`
+
+      //Displays in #result in HTML
       document.getElementById("result").innerHTML = resultRandom;
       
 });
