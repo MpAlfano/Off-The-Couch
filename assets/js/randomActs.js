cee564a81cf4292bb683a48664ab3aef772851fa
Jacqueline Hodgson <jacqueline.e.hodgson@gmail.com>
2022-02-02 10:30:14 -0500

    made semantic updates

--- assets/js/randomActs.js
+++ assets/js/randomActs.js
@@ -1,79 +1,17 @@
-{
-    "activity": "Mow your lawn",
-    "participants": 1,
-    "price": 0.1,
-    "link": "",
-    "key": "3590127",
-  },
-  {
-    "activity": "Mow your lawn",
-    "type": "busywork",
-    "participants": 1,
-    "price": 0.1,
-    "link": "",
-    "key": "3590127",
-    "accessibility": 0.3
-  },
-  {
-    "activity": "Mow your lawn",
-    "type": "busywork",
-    "participants": 1,
-    "price": 0.1,
-    "link": "",
-    "key": "3590127",
-    "accessibility": 0.3
-  },
-  {
-    "activity": "Mow your lawn",
-    "type": "busywork",
-    "participants": 1,
-    "price": 0.1,
-    "link": "",
-    "key": "3590127",
-    "accessibility": 0.3
-  },
-  {
-    "activity": "Mow your lawn",
-    "type": "busywork",
-    "participants": 1,
-    "price": 0.1,
-    "link": "",
-    "key": "3590127",
-    "accessibility": 0.3
-  },
-  {
-    "activity": "Mow your lawn",
-    "type": "busywork",
-    "participants": 1,
-    "price": 0.1,
-    "link": "",
-    "key": "3590127",
-    "accessibility": 0.3
-  },
-  {
-    "activity": "Mow your lawn",
-    "type": "busywork",
-    "participants": 1,
-    "price": 0.1,
-    "link": "",
-    "key": "3590127",
-    "accessibility": 0.3
-  },
-  {
-    "activity": "Mow your lawn",
-    "type": "busywork",
-    "participants": 1,
-    "price": 0.1,
-    "link": "",
-    "key": "3590127",
-    "accessibility": 0.3
-  },
-  {
-    "activity": "Mow your lawn",
-    "type": "busywork",
-    "participants": 1,
-    "price": 0.1,
-    "link": "",
-    "key": "3590127",
-    "accessibility": 0.3
-  }
+let activities = ["Canoeing","Hiking","Biking","Museum","Art Gallery","Movie","Painting","Boardgame","Restaurant","Swimming","Chess","Watching Sports","Skiing","Skateboarding","Pottery","Archery","Cooking Class","Axe Throwing","Escape Room","Tennis","Gardening","Football","Drawing","See A Show","Volunteer","Gym","Rockclimbing","Parkour Gym","Squash","Meditation Class","Yoga Class","Martial Arts Class"];
+
+// restaurant, chess, boardgames
+
+/* localStorage function for user search results
+const saveResults = (query, result) => {
+  localStorage.setItem(query, result);
+}
+saveResults(query,result);
+*/
+
+/* localStorage function for random search results
+const saveRandomResults = (result) => {
+  localStorage.setItem(data.activity, result);
+}
+saveRandomResults(result);
+*/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,57 @@
 // Opens Modal on click of "search topic" button
 
-
-
 $(document).ready(function(){
-    $("#modBtn").click(function(){
-      $("#searchAct").modal();
+    $(`#modBtn`).click(function(){
+      $(`#searchAct`).modal();
     });
-  });
-  
+});
   
   // Closes modal on click of "search" button
-  $('#saveSearch').click(function(e) {
+  $(`#saveSearch`).click(function(e) {
     e.preventDefault();
-    $('#searchAct').modal('toggle');
+    $(`#searchAct`).modal(`toggle`);
     return false;
   });
-
-
-
-  
-
 
 //Selected Activity using serpstackAPI(uses google search)
 //use res. "console.log ID"
 //Grabbing from organic_results
 //Search Button on click
 function searchQuery(searchCity) {
-  $('#saveSearch').on('click', function(e){
+  $(`#saveSearch`).on(`click`, function(e){
   e.preventDefault()
 
-  var query = $("#searchQuery").val()  //Grabs value from searchQuery ID in HTML
-  let activities = "+" + "activities"; // " " + latitude+ " " +longitude; //Adds "activities" to the search
-  let queryA = query + activities + '+' + searchCity; //What were actually searching
-  var API_KEY = '2b8d450902150d2d261c0e097158851e' //serpstackAPI KEY
-  let result=''
+  var query = $(`#searchQuery`).val();  //Grabs value from searchQuery ID in HTML
+  let activities = `+` + `activities`; // " " + latitude+ " " +longitude; //Adds "activities" to the search
+  let queryA = query + activities + `+` + searchCity; //What were actually searching
+  var API_KEY = `87cf9406de06e84ff7285b4fddfaedfc`; //serpstackAPI KEY
+  let result=``;
 
-  var url = 'http://api.serpstack.com/search?access_key='  + API_KEY+"&type=web&num=1&google_domain=google.ca"+"&query=" +queryA
+  var url = `http://api.serpstack.com/search?access_key=` + API_KEY + `&type=web&num=1&google_domain=google.ca` + `&query=` + queryA;
   console.log(url);
   $.get(url, function(data){
-      $("#result").html('')
-      console.log(data)
-      console.log(queryA)
+      $(`#result`).html(``);
+      console.log(data);
+      console.log(queryA);
       data.organic_results.forEach(res => {
         //What will be displayed
           result =`
           <h3>${res.title}</h3><br><a target="_blank" href="${res.url}">${res.url}</a>
           <p>${res.snippet}</p>
-          `
+          `;
           //Appends to #result in HTML
-          $("#result").append(result)
+          $(`#result`).append(result);
       });
   });
-});
+  });
 }
-
-
-
 // Random activity using BoredAPI
 // use data."console.log ID"
-let boredUrl = "https://www.boredapi.com/api/activity/"
+let boredUrl = `https://www.boredapi.com/api/activity/`;
 
 //Random button on click
-$('#randomQ').on('click', function(e){
-  e.preventDefault()
+$(`#randomQ`).on(`click`, function(e){
+  e.preventDefault();
   fetch(boredUrl)
     .then(function (response) {
       console.log(response);
@@ -76,38 +65,33 @@ $('#randomQ').on('click', function(e){
     .then(function (data) {
       console.log(data);
       //What will be displayed
-      resultRandom = `<h3>${data.activity}</h3><p><a target="_blank" href="${data.link}">${data.link}</a></p>`
-
+      resultRandom = `<h3>${data.activity}</h3><p><a target="_blank" href="${data.link}">${data.link}</a></p>`;
       //Displays in #result in HTML
-      document.getElementById("result").innerHTML = resultRandom;
-      
-});
+      document.getElementById(`result`).innerHTML = resultRandom;
+    });
 });
 
-
-const fetchLocationName =  (lat,lng) => {
+const fetchLocationName = (lat,lng) => {
   navigator.geolocation.getCurrentPosition((position) => {
     const lat  = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log(lat)
-    console.log(lng)
+    console.log(lat);
+    console.log(lng);
  
    fetch(
-    'https://www.mapquestapi.com/geocoding/v1/reverse?key=PRZUpttP0TCp8zsRVAIyHZz7mpmjIupR&location='+lat+'%2C'+lng+'&outFormat=json&thumbMaps=false',
+    `https://www.mapquestapi.com/geocoding/v1/reverse?key=PRZUpttP0TCp8zsRVAIyHZz7mpmjIupR&location=` + lat + `%2C` + lng + `&outFormat=json&thumbMaps=false`,
   )
     .then((response) => response.json())
     .then((responseJson) => {
-      console.log(responseJson)
-      console.log(responseJson.results[0].locations[0].adminArea5)
+      console.log(responseJson);
+      console.log(responseJson.results[0].locations[0].adminArea5);
       console.log(
-        'ADDRESS GEOCODE is BACK!! => ' + JSON.stringify(responseJson),
+        `ADDRESS GEOCODE is BACK!! => ` + JSON.stringify(responseJson),
         searchCity = responseJson.results[0].locations[0].adminArea5
-        
-        
       );
-      searchQuery(searchCity)
+      searchQuery(searchCity);
     });
   });
-};
+}
 
-fetchLocationName()
+fetchLocationName();
